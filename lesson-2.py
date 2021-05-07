f6c4378d4cc998d8f0b582d5357b29f88a2c23d3
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-05-07 19:03:01 +0500

    Update lesson-2.py

--- lesson-2.py
+++ lesson-2.py
@@ -74,3 +74,34 @@ user_list = user_input.split()
 for count, value in enumerate(user_list):
 
     print(f'Строка № {count} - {value[0:10]}')
+
+
+# 5. Реализовать структуру «Рейтинг», представляющую собой не возрастающий набор
+# натуральных чисел. #
+# У пользователя необходимо запрашивать новый элемент рейтинга. #
+# Если в рейтинге существуют элементы с одинаковыми значениями, #
+# то новый элемент с тем же значением должен разместиться после них.
+# Подсказка. Например, набор натуральных чисел: 7, 5, 3, 3, 2.
+# Пользователь ввел число 3. Результат: 7, 5, 3, 3, 3, 2.
+# Пользователь ввел число 8. Результат: 8, 7, 5, 3, 3, 2.
+# Пользователь ввел число 1. Результат: 7, 5, 3, 3, 2, 1.
+# Набор натуральных чисел можно задать непосредственно в коде,
+# например, my_list = [7, 5, 3, 3, 2].
+
+number = int(input("Введите число "))
+my_list = [7, 4, 3, 2]
+c_param = my_list.count(number)
+
+for element in my_list:
+    if c_param > 0:
+        i_param = my_list.index(number)
+        my_list.insert(i_param + 1, number)
+        break
+    else:
+        if number > element:
+            j_param = my_list.index(element)
+            my_list.insert(j_param, number)
+            break
+        elif number < my_list[len(my_list) - 1]:
+            my_list.append(number)
+print(my_list)
