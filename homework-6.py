f481eb86673ff8a6404d7560c3d29a18e31b0962
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-05-22 20:30:40 +0500

    Update homework-6.py

--- homework-6.py
+++ homework-6.py
@@ -36,3 +36,28 @@ class TrafficLight():
 
 svetofor = TrafficLight('')
 print(svetofor.running())
+
+# 2. Реализовать класс Road (дорога), в котором определить атрибуты:
+# length (длина), width (ширина).
+# Значения данных атрибутов должны передаваться при создании экземпляра класса.
+# Атрибуты сделать защищенными.
+# Определить метод расчета массы асфальта, необходимого для покрытия всего дорожного полотна.
+# Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв метра дороги
+# асфальтом, толщиной в 1 см * число см толщины полотна. Проверить работу метода.
+# Например: 20м * 5000м * 25кг * 5см = 12500 т
+
+class Road():
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def massa_asphalt(self, massa, tolshina):
+        self.massa = massa
+        self.tolshina = tolshina
+        result = self.__width * self.__length * self.massa * self.tolshina
+        return int(result / 1000)
+
+R = Road(70, 7000)
+
+print(f'Масса асфальта, необходимого для покрытия всего дорожного полотна  \n = {R.massa_asphalt(25, 5)}')
+
