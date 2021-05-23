10662db539879a1c164d4a12345889c6010e77b7
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-05-23 13:38:04 +0500

    Update homework-6.py

--- homework-6.py
+++ homework-6.py
@@ -61,3 +61,37 @@ R = Road(70, 7000)
 
 print(f'Масса асфальта, необходимого для покрытия всего дорожного полотна  \n = {R.massa_asphalt(25, 5)}')
 
+# 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+# name, surname, position (должность), income (доход).
+# Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы:
+# оклад и премия, например, {"wage": wage, "bonus": bonus}.
+# Создать класс Position (должность) на базе класса Worker.
+# В классе Position реализовать методы получения полного имени сотрудника
+# (get_full_name) и дохода с учетом премии (get_total_income).
+# Проверить работу примера на реальных данных (создать экземпляры класса Position,
+# передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+
+class Worker():
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return  'Full name is '+ self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        income = self._income.values()
+        return sum(income)
+
+Jober = Position('Vladimir', 'Putin', 'The President', 190000, 40000)
+
+print(Jober.name)
+print(Jober.position)
+print(Jober.get_full_name())
+print(Jober.get_total_income())
+
