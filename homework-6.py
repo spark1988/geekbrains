23ea1f38a2cb7a9d41973c0171930ba44d46edb4
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-05-23 20:28:29 +0500

    Update homework-6.py

--- homework-6.py
+++ homework-6.py
@@ -95,3 +95,81 @@ print(Jober.position)
 print(Jober.get_full_name())
 print(Jober.get_total_income())
 
+# 4. Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты:
+# speed, color, name, is_police (булево).
+# А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала,
+# остановилась, повернула (куда). Опишите несколько дочерних классов:
+# TownCar, SportCar, WorkCar, PoliceCar.
+# Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость
+# автомобиля. Для классов TownCar и WorkCar переопределите метод show_speed.
+# При значении скорости свыше 60 (TownCar) и 40 (WorkCar)
+# должно выводиться сообщение о превышении скорости.
+# Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к
+# атрибутам, выведите результат. Выполните вызов методов и также покажите результат.
+class Car():
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+    def go(self):
+        return f' {self.name} машинка тронулась эрон-дон-дон'
+
+    def stop(self):
+        return f' {self.name} стоп мотор, машина остановилась'
+
+    def turn(self, side):
+        self.side = side
+        if side == 'left':
+            return f' {self.name} машина повернула налево'
+        if side == 'right':
+            return f' {self.name} машина повернула направо'
+    def show_speed(self):
+        if self.speed > 60 and self.is_police != True:
+            return 'Вы превышаете скорость'
+        else:
+            return self.speed
+
+class Towncar(Car):
+
+    def show_speed(self):
+        if self.speed > 60:
+            print(f'превышение {self.name} на {self.speed - 60}')
+        else:
+            print(f'Скорость {self.speed} допустимая для {self.name}')
+
+class Workcar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            print(f'превышение {self.name} на {self.speed - 40}')
+        else:
+            print(f'Скорость {self.speed} допустимая для {self.name}')
+
+class PoliceCar(Car):
+        def sirens(self):
+                if self.is_police == True:
+                    print('Это полиция')
+                    time.sleep(1)
+                    print('мигалка синяя')
+                    time.sleep(1)
+                    print('мигалка красная')
+                    time.sleep(1)
+                    print('остановите машину!')
+        def docs(self):
+                return 'ваши документики'
+        def arrest(self):
+
+            if worker_car.speed > 40:
+                print(f'Вы арестованы {worker_car.name} = {worker_car.speed}')
+            if city_rider.speed > 60:
+                print(f'Вы довыделывались {city_rider.name} = {city_rider.speed}')
+
+
+
+police_car = PoliceCar(190, 'black', 'Lamborgini', True)
+city_rider = Towncar(120,'blue','Infinity QX65', False)
+worker_car = Workcar(60, 'yellow', 'Volvo Truck', False)
+worker_car.speed = 40
+print(worker_car.speed)
+print(police_car.arrest())
+
