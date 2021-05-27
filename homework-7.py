54891c49e74c82b75254517888c94cce8bf83ad0
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-05-27 11:54:50 +0500

    Update homework-7.py

--- homework-7.py
+++ homework-7.py
@@ -57,3 +57,42 @@ m = Matrix(matrica)
 
 print(matrica , matrica2)
 print(m.__add__(matrica + matrica2))
+
+# 2. Реализовать проект расчета суммарного расхода ткани на производство одежды.
+# Основная сущность (класс) этого проекта — одежда, которая может иметь определенное название.
+# К типам одежды в этом проекте относятся пальто и костюм.
+# У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма).
+# Это могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы:
+# для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3).
+# Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани.
+# Проверить на практике полученные на этом уроке знания:
+# реализовать абстрактные классы для основных классов проекта,
+# проверить на практике работу декоратора @property.
+class Clothes():
+    def __init__(self, name):
+        self.name = name
+
+class Coat(Clothes):
+    def __init__(self, size):
+        self.size = size
+    @property
+    def consumption(self):
+        result_coat = (self.size/6.5 + 0.5)
+        return result_coat
+
+class Suit(Clothes):
+    def __init__(self, height):
+        self.height = height
+
+    @property
+    def consumption(self):
+        result_suit = (2 * self.height + 0.3)
+        return result_suit
+
+c = Coat(23)
+b = Suit(45)
+
+print(c.consumption)
+print(b.consumption)
