396633ba0b85257f94aa6438d4db259c234460dd
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-05-29 23:58:20 +0500

    Update homework-8.py

--- homework-8.py
+++ homework-8.py
@@ -100,4 +100,122 @@ class New:
 N = New(2)
 print(N.my_input())
 
+# 4. Начните работу над проектом «Склад оргтехники».
+# Создайте класс, описывающий склад. А также класс «Оргтехника»,
+# который будет базовым для классов-наследников. Эти классы — конкретные типы оргтехники
+# (принтер, сканер, ксерокс). В базовом классе определить параметры,
+# общие для приведенных типов.
+# В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
+# 5. Продолжить работу над первым заданием.
+# Разработать методы, отвечающие за приём оргтехники на склад и передачу в определенное
+# подразделение компании. Для хранения данных о наименовании и количестве единиц оргтехники,
+# а также других данных, можно использовать любую подходящую структуру, например словарь.
+# 6. Продолжить работу над вторым заданием. Реализуйте механизм валидации вводимых
+# пользователем данных. Например, для указания количества принтеров, отправленных на склад,
+# нельзя использовать строковый тип данных.
+# Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники»
+# максимум возможностей, изученных на уроках по ООП.
+
+class StoreMashines:
+
+    def __init__(self, name, price, quantity, number_of_lists):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.numb = number_of_lists
+        self.my_store_full = []
+        self.my_store = []
+        self.my_unit = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+
+    def reception(self):
+        try:
+            unit = input(f'Введите наименование ')
+            unit_p = int(input(f'Введите цену за ед '))
+            unit_q = int(input(f'Введите количество '))
+            unique = {'Модель устройства': unit, 'Цена за ед': unit_p, 'Количество': unit_q}
+            self.my_unit.update(unique)
+            self.my_store.append(self.my_unit)
+            print(f'Текущий список -\n {self.my_store}')
+        except:
+            return f'Ошибка ввода данных'
+
+        print(f'Для выхода - Q, продолжение - Enter')
+        q = input(f'---> ')
+        if q.lower() == 'q':
+            self.my_store_full.append(self.my_store)
+            print(f'Весь склад -\n {self.my_store_full}')
+            return f'Выход'
+        else:
+            return StoreMashines.reception(self)
+
+
+class Printer(StoreMashines):
+    def to_print(self):
+        while True:
+            a =  input('*' * 10 + ' have a nice typing ' + '*' * 10 + '\n')
+            print("\033[34m {}".format(a))
+            b = input('остановить безумие Y')
+            if b == 'y':
+                break
+
+
+class Scanner(StoreMashines):
+    def to_scan(self):
+        import time
+
+        print ("Scanning.")
+
+        print ("Scanning.   ")
+        time.sleep(1)
+        print ("\rScanning..  ")
+        time.sleep(1)
+        print ("\rScanning... ")
+        time.sleep(1)
+        print ("\rScanning....")
+        time.sleep(1)
+        return ("\rКонец.   ")
+
+
+unit_1 = Printer('hp', 2000, 5, 10)
+unit_2 = Scanner('Canon', 1200, 5, 10)
+
+# print(unit_1.reception())
+# # print(unit_2.reception())
+#
+# print(unit_1.to_print())
+# print(unit_2.to_scan())
+
+
+# Реализовать проект «Операции с комплексными числами». Создайте класс «Комплексное число»,
+# реализуйте перегрузку методов сложения и умножения комплексных чисел.
+# Проверьте работу проекта, создав экземпляры класса (комплексные числа) и
+# выполнив сложение и умножение созданных экземпляров.
+# Проверьте корректность полученного результата.
+
+
+
+class ComplexNumber:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма z1 и z2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение z1 и z2 равно')
+        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'z = {self.a} + {self.b} * i'
+
+
+z_1 = ComplexNumber(1, -2)
+z_2 = ComplexNumber(3, 4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
+
 
