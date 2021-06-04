98d52d0aa7fcc5526b315eeb54230f40a6593fdf
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-06-05 00:57:10 +0500

    Update algorithms-2.py

--- algorithms-2.py
+++ algorithms-2.py
@@ -58,3 +58,13 @@ rev_number = list(map(str, number))
 rev_number.reverse()
 
 print(''.join(rev_number))
+
+# 4. Найти сумму n элементов следующего ряда чисел: 1, -0.5, 0.25, -0.125,…
+# Количество элементов (n) вводится с клавиатуры.
+ask = int(input('введите кол-во элементов для ряда 1, -0.5, 0.25, -0.125,…'))
+s = 0
+num = 1
+while s < ask:
+    print(num)
+    num /= -2
+    s += 1
