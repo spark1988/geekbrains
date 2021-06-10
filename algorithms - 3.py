b431b1e904e4b8525cea79555fd364f9ea0112fb
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-06-11 00:59:32 +0500

    Update algorithms - 3.py

--- algorithms - 3.py
+++ algorithms - 3.py
@@ -29,6 +29,31 @@ for i in a_massive:
     ind += 1
 print(result)
 
+# 3. В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+import random
+
+List = [1,2,3,4,5,6,7,8,9]
+
+random.shuffle(List)
+
+ind = 0
+print(List)
+for i in List:
+
+    if i == max(List):
+
+        b = List.index(i)
+        print(List.index(i))
+
+    if i == min(List):
+
+        a = List.index(i)
+        print(List.index(i))
+
+List[b], List[a] = List[a], List[b]
+
+print(List)
+
 
 
 
