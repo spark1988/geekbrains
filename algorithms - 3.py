b7496fa994df455a277e717837f44747a4488dd4
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-06-11 13:24:24 +0500

    Update algorithms - 3.py

--- algorithms - 3.py
+++ algorithms - 3.py
@@ -78,7 +78,20 @@ print(List[j + 1:k - 1])
 print(sum(List[j + 1:k - 1]))
 
 
+# 7. В одномерном массиве целых чисел определить два наименьших элемента.
+# Они могут быть как равны между собой (оба минимальны), так и различаться.
 
+List = [1, 2, 3, 9, 5, 6, 7, 8, 2, -2]
+result = []
+
+min_num_1 = min(map(int, List))
+List.remove(min_num_1)
+result.append(min_num_1)
+min_num_2 = min(map(int, List))
+result.append(min_num_2)
+
+print(List)
+print(result)
 
 
 
