7d2ea355329d93f87fbdfac0ef9b3b4c6f3a8832
Vlad_Vologodskiy <vladislav.spark@yandex.ru>
2021-06-11 12:11:38 +0500

    Update algorithms - 3.py

--- algorithms - 3.py
+++ algorithms - 3.py
@@ -54,7 +54,12 @@ List[b], List[a] = List[a], List[b]
 
 print(List)
 
+# 4. Определить, какое число в массиве встречается чаще всего.
+List = [1,2,3,4,5,6,7,8,9,1,2,2, 2]
+frequency = max(map(List.count, List))
+print(f'{max(a for a in List if List.count(a) == frequency)} то самое число')
 
+print(f'{frequency} количество повторений этого числа в списке')
 
 
 
